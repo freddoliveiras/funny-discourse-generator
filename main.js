253107ff47cbeeb75acefbd0e369081a01a03af6
Frederico Sarmento <frederico.sarmento@outlook.com>
2022-01-08 19:50:43 -0300

    Adjusts

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// The essential pieces
+//Declare arrays with the pieces of text.
 const piece1 = [
     'Dear people of my country, ',
     'We should never forget that ',
@@ -26,7 +26,7 @@ const piece3 = [
     'plays a fundamental role in the formulation ',
     'requires precision and definition ',
     'offers an excellent opportunity for the verification ',
-    'takes an important position in the definition ',
+    'takes position in the configuration ',
     'emphasize the importance '
 ];
 const piece4 = [
@@ -37,43 +37,51 @@ const piece4 = [
     'of the government attributions.',
     'of the procedures.'
 ];
-//Stores the pieces with randomized elements
+
+//Declare randomized pieces arrays
 let seq1 = [];
 let seq2 = [];
 let seq3 = [];
 let seq4 = [];
+
+//Declare the string for the output of the discourse
+let discourseString = '';
+
+//Generates a random id
 function randId(numElements) {
     return Math.floor(Math.random() * (numElements));
 }
-let speechString = '';
+
+//Populate the randomized arrays.
 do {
     let currRandId = randId(12);
     if (!seq1.includes(currRandId)) {
         seq1.push(currRandId);
     }
 } while (seq1.length <= 5);
-
 do {
-    let currIndex = randId(6);
-    if (!seq2.includes(currIndex)) {
-        seq2.push(currIndex);
+    let currRandId = randId(6);
+    if (!seq2.includes(currRandId)) {
+        seq2.push(currRandId);
     }
 } while (seq2.length <= 5);
-
 do {
-    let currIndex = randId(6);
-    if (!seq3.includes(currIndex)) {
-        seq3.push(currIndex);
+    let currRandId = randId(6);
+    if (!seq3.includes(currRandId)) {
+        seq3.push(currRandId);
     }
 } while (seq3.length <= 5);
 do {
-    let currIndex = randId(6);
-    if (!seq4.includes(currIndex)) {
-        seq4.push(currIndex);
+    let currRandId = randId(6);
+    if (!seq4.includes(currRandId)) {
+        seq4.push(currRandId);
     }
 } while (seq4.length <= 5);
 
+//Create the string with randomized array elements
 for (let i = 0; i <= 5; i++) {
-    speechString = speechString + piece1[seq1[i]] + piece2[seq2[i]] + piece3[seq3[i]] + piece4[seq4[i]] + '\n'
+    discourseString = discourseString + piece1[seq1[i]] + piece2[seq2[i]] + piece3[seq3[i]] + piece4[seq4[i]] + '\n'
 }
-console.log(speechString);
+
+//log the discourse
+console.log(discourseString);
